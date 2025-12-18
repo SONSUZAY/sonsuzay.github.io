69d169ae81bb76e0c91eafdcb1de5fd689fe2100
SONSUZAY <serkancosan639@gmail.com>
2025-12-19 00:51:43 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,45 @@
-const CACHE_NAME = 'sonsuzay-v2';
+const CACHE_NAME = 'sonsuzay-v1'; // Güncelleme yaptıkça burayı v2, v3 yap!
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/oyunlar.html',
-  '/uygulamalar.html',
-  '/indir.html',
-  '/player.html',
-  '/saat.html',
-  '/site_simge.png',
-  '/manifest.json'
+  './',
+  './index.html',
+  './oyunlar.html',
+  './uygulamalar.html',
+  './indir.html',
+  './player.html',
+  './saat.html',
+  './site_simge.png',
+  './manifest.json'
 ];
 
-// Yükleme (Install) sırasında dosyaları önbelleğe al
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Yeni sürüm gelince bekleme, hemen yükle
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Dosyalar önbelleğe alınıyor...');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// İstek (Fetch) sırasında önbellekten sun
-self.addEventListener('fetch', function(event) {
+self.addEventListener('activate', (event) => {
+  // Eski sürümleri (cache) temizle
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim(); // Sayfanın kontrolünü hemen ele al
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Önbellekte varsa onu döndür, yoksa internetten çek
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
