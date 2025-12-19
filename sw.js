52124151c8e7c9580c97936123551f3af54933a0
SONSUZAY <serkancosan639@gmail.com>
2025-12-19 20:12:37 +0300

    Update CACHE_NAME to version v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sonsuzay-v1'; // Güncelleme yaptıkça burayı v2, v3 yap!
+const CACHE_NAME = 'sonsuzay-v2'; // Güncelleme yaptıkça burayı v2, v3 yap!
 const urlsToCache = [
   './',
   './index.html',
