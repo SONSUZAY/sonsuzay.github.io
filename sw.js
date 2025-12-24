0c398639bed20be4f1e2dfafa8076b7e626618f1
SONSUZAY <serkancosan639@gmail.com>
2025-12-24 16:12:17 +0300

    Update CACHE_NAME to 'sonsuzay-v9'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sonsuzay-v8'; // Güncelleme yaptıkça burayı v2, v3 yap!
+const CACHE_NAME = 'sonsuzay-v9'; // Güncelleme yaptıkça burayı v2, v3 yap!
 const urlsToCache = [
   './',
   './index.html',
